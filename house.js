82beea2153564dc0e942a7c5cfe1f82fe3e29ed7
rahid <hidayat.85@gmail.com>
2017-08-01 19:35:11 +0700

    car n house

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(userHouse) {
+    this.address = userHouse['address']
+    this.square_feet = userHouse['square_feet']
+    this.num_bedrooms = userHouse['num_bedrooms'] || 3
+    this.num_baths = userHouse['num_baths'] || 2
+    this.cost = userHouse['cost'] || 320000
+    this.down_payment = userHouse['down_payment'] || 0.20
+    this.sold = userHouse['sold'] || false
+    this.short_sale = userHouse['short_sale']
+    this.has_tenants = userHouse['has_tenants'] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -29,10 +29,23 @@ class House {
   }
 
   to_s() {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. Rp.${this.cost}, sold:${this.sold}`
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+var Rumah = {
+  address : 'Jl. Pramuka No. 12 Pondok Indah Jakarta Selatan',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 12345,
+  down_payment : 12345,
+  sold : true,
+  short_sale : true,
+  has_tenants : false,
+}
+
+// const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const cool = new House(Rumah)
 
 console.log(cool.to_s())
