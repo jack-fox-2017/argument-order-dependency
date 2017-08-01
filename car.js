45081545db960fbd9f1650583393cee8b3f20371
alfandash <cumiasem91@gmail.com>
2017-08-01 15:57:50 +0700

    Release1 done`

--- car.js
+++ car.js
@@ -3,17 +3,25 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights']) {
+    console.log(options);
+
+    if (options.hasOwnProperty(`headlights`)) {
       this.headlights = options['headlights']
     } else {
       this.headlights = true
     }
 
-    if (options && options['gear']) {
-      this.gear = options['gear']
-    } else {
-      this.gear = 1
-    }
+    // if (options && options['headlights']) {
+    //   this.headlights = options['headlights']
+    // } else {
+    //   this.headlights = true
+    // }
+
+    // if (options && options['gear']) {
+    //   this.gear = options['gear']
+    // } else {
+    //   this.gear = 1
+    // }
   }
 }
 
@@ -21,7 +29,6 @@ const herbie = new Car()
 
 // When we start the car, the headlights should be on by default
 herbie.start_car()
-
 console.log(herbie.headlights) // true
 
 // But what happens when we want to start without headlights?
