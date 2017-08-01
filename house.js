1b026a60e108039a3563942a8758683aebdcba1e
ahmadaidil <ahmdaidil@gmail.com>
2017-08-01 15:35:59 +0700

    aod selesai

--- house.js
+++ house.js
@@ -2,20 +2,50 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(components) {
+    this.address = components['address'];
+    this.square_feet = components['square_feet'];
+    if(components.hasOwnProperty('num_bedrooms')){
+      this.num_bedrooms = components['num_bedrooms'];
+    }
+    else{
+      this.num_bedrooms = 3;
+    }
+    if(components.hasOwnProperty('num_baths')){
+      this.num_baths = components['num_baths'];
+    }
+    else{
+      this.num_baths = 2;
+    }
+    if(components.hasOwnProperty('cost')){
+      this.cost = components['cost'];
+    }
+    else{
+      this.cost = 320000;
+    }
+    if(components.hasOwnProperty('down_payment')){
+      this.down_payment = components['down_payment'];
+    }
+    else{
+      this.down_payment = 0.20;
+    }
+    if(components.hasOwnProperty('sold')){
+      this.sold = components['sold'];
+    }
+    else{
+      this.sold = false;
+    }
+    this.short_sale = components['short_sale'];
+    if(components.hasOwnProperty('has_tenants')){
+      this.has_tenants = components['has_tenants'];
+    }
+    else{
+      this.has_tenants = false;
+    }
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +63,10 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let param = {address:'address',square_feet:100,num_bedrooms:2,num_baths:2,cost:12345,down_payment:12345,sold:true,short_sale:true,has_tenants:true};
+const cool = new House(param);
 
 console.log(cool.to_s())
+// let o = new Object();
+// o.prop = 'exists';
+// console.log(o.hasOwnProperty('prop'));
