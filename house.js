61ef5e1c599ec249ce1b77e4e5acd70a1d750f62
anggraito <anggraito.laras@gmail.com>
2017-08-01 19:02:53 +0700

    running the code in car and house file

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(components) {
+    this.address = components['address']  //also can components.address
+    this.square_feet = components['square_feet']
+    this.num_bedrooms = components['num_bedrooms'] || 3
+    this.num_baths = components.num_baths || 2
+    this.cost = components.cost || 320000
+    this.down_payment = components.down_payment || 0.20
+    this.sold = components.sold || false
+    this.short_sale = components.short_sale
+    this.has_tenants = components.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,17 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let spesification = {
+  address : 'Jl.Kebon Baru',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 1234,
+  down_payment : 1234,
+  sold : true,
+  short_sale : true
+}
+
+const cool = new House(spesification)
 
 console.log(cool.to_s())
